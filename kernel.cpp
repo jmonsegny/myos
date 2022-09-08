1a44b73f81be326cc3252f021845a72a3ef02f4c
jmonsegny <jmonsegny@hotmail.com>
2022-09-08 10:34:36 -0600

    printf with enter

--- kernel.cpp
+++ kernel.cpp
@@ -3,10 +3,31 @@
 
 void printf( int8_t* str )
 {
-	uint16_t* VideoMemory = (uint16_t*)0xb8000;
+	static uint16_t* VideoMemory = (uint16_t*)0xb8000;
 
-	for( int32_t i = 0; str[i] != '\0'; i++ )
-		VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i];
+	static uint8_t x = 0, y = 0;
+
+	for( int32_t i = 0; str[i] != '\0'; i++ ) {
+		if( str[i] == '\n' ) {
+			y++;
+			x = 0;			
+		} else {			
+			VideoMemory[y*80+x] = (VideoMemory[y*80+x] & 0xFF00) | str[i];
+			x++;
+		}
+
+		if( x >= 80 ) {
+			y++;
+			x = 0;
+		}
+
+		if( y >= 25 ) {
+			for( int j = 0; j < 25*80; j++ )
+				VideoMemory[j] = (VideoMemory[i] & 0xFF00) | ' ';
+			x = 0;
+			y = 0;
+		}
+	}
 }
 
 typedef void (*constructor)();
@@ -20,7 +41,8 @@ extern "C" void callConstructors()
 
 extern "C" void kernelMain( void* multiboot_structure, uint32_t magicnumber )
 {
-	printf( "Hello World! --- http://www.AlgorithMan.de" );
+	printf( "Hello World! --- http://www.AlgorithMan.de\n" );
+	printf( "Hello World! --- http://www.AlgorithMan.de\n" );
 
 	GlobalDescriptorTable gdt;
 
