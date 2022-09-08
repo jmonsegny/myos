820bce6ed26d7da2195566bfaf99a8b66f8332c9
jmonsegny <jmonsegny@hotmail.com>
2022-09-08 00:07:02 -0600

    GDT class

--- kernel.cpp
+++ kernel.cpp
@@ -1,5 +1,5 @@
-
 #include "types.h"
+#include "gdt.h"
 
 void printf( int8_t* str )
 {
@@ -9,9 +9,20 @@ void printf( int8_t* str )
 		VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i];
 }
 
+typedef void (*constructor)();
+extern "C" constructor start_ctors;
+extern "C" constructor end_ctors;
+extern "C" void callConstructors()
+{
+	for( constructor i = start_ctors; i != end_ctors; i++ )
+		(*i)();
+}
+
 extern "C" void kernelMain( void* multiboot_structure, uint32_t magicnumber )
 {
 	printf( "Hello World! --- http://www.AlgorithMan.de" );
+
+	GlobalDescriptorTable gdt;
 
 	while(1);
 }
